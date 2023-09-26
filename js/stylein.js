39e6f1a7918b8d31da694b2671c061161f2fc9f4
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-26 12:15:09 +0600

    classlist and style practiced

---
+++ js/stylein.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll("section")
+
+for(const section of sections){
+    section.style.border = " 2px solid black"
+    section.style.marginBottom = "10px"
+    section.style.borderRadius = '15px'
+    section.style.padding = "10px"
+}
+
+const places = document.getElementById("places-container")
+
+places.classList.add("yellow-bg")
+places.classList.add("text-center")
+places.classList.add('text-large')
+places.classList.remove('text-large')
+// places.classList.remove("text-center")
